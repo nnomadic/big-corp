d9924bb4787aa7ed45c8183d20010513f73ab238
Wisam Halawi <w_halawi@hotmail.co.uk>
2020-08-14 18:05:59 +0100

    styled contact form

--- src/modules/ContactForm.vue
+++ src/modules/ContactForm.vue
@@ -1,8 +1,20 @@
 <template>
-  <form>
-    <input v-model="message" placeholder="Enter your name" />
-    <input v-model="message" placeholder="Enter your email" />
-    <textarea v-model="message" placeholder="Enter your message"></textarea>
+  <form class="contact-form">
+    <input
+      v-model="message"
+      placeholder="Enter your name"
+      class="input__name"
+    />
+    <input
+      v-model="message"
+      placeholder="Enter your email"
+      class="input__email"
+    />
+    <textarea
+      v-model="message"
+      placeholder="Enter your message"
+      class="input__body"
+    ></textarea>
     <button class="btn" type="submit">Submit</button>
   </form>
 </template>
@@ -11,4 +23,29 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.contact-form {
+  display: flex;
+  flex-flow: column;
+  padding: 50px;
+
+  .btn {
+    width: fit-content;
+    align-self: flex-end;
+  }
+}
+
+.input {
+  &__name,
+  &__email {
+    width: 50%;
+    height: 40px;
+    margin: 20px 0;
+  }
+
+  &__body {
+    min-height: 300px;
+    margin: 20px 0;
+  }
+}
+</style>
