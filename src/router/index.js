69b4bdf836797ecdc66ec4370dc84d1a87b66147
Wisam Halawi <w_halawi@hotmail.co.uk>
2020-08-14 02:26:52 +0100

    basic boilerplate

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import News from "../views/News.vue";
+import Contact from "../views/Contact.vue";
 
 const routes = [
   {
@@ -8,13 +10,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/news",
+    name: "News",
+    component: News
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: Contact
   }
 ];
 
