e9b8740fc559a14f79181d6da27c9e880007afce
Wisam Halawi <w_halawi@hotmail.co.uk>
2020-08-14 14:42:51 +0100

    login modal functional

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,49 @@
 <template>
   <nav id="nav">
-    <router-link to="/"><button class="btn">Home</button></router-link>
-    <router-link to="/news"><button class="btn">News</button></router-link>
-    <router-link to="/contact"
-      ><button class="btn">Contact</button></router-link
-    >
-    <button class="btn">
-      Login
-    </button>
+    <div>
+      <router-link to="/"><button class="btn">Home</button></router-link>
+      <router-link to="/news"><button class="btn">News</button></router-link>
+      <router-link to="/contact"
+        ><button class="btn">Contact</button></router-link
+      >
+    </div>
+    <div>
+      <button class="btn" @click="showLoginModal = true">
+        Login
+      </button>
+    </div>
+
+    <div id="loginModal" v-if="showLoginModal">
+      <transition name="modal">
+        <div class="modal-mask">
+          <div class="modal-wrapper">
+            <div class="modal-container">
+              <div class="modal-body">
+                <input placeholder="Username" />
+                <input placeholder="Password" />
+              </div>
+              <div class="modal-footer">
+                <button
+                  class="modal-default-button"
+                  @click="showLoginModal = false"
+                >
+                  Sign in
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </div>
   </nav>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showLoginModal: false,
+    };
+  },
+};
+</script>
